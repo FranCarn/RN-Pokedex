05f80ed753955bf646672527d9c8bf4e0a31a6d6
@FranCarn <carnevalefranco99@gmail.com>
2023-11-22 22:20:11 -0300

    unnecesary

--- src/screens/PokemonScreen.tsx
+++ src/screens/PokemonScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
 
 export const PokemonScreen = () => {
   return (
@@ -8,3 +8,4 @@ export const PokemonScreen = () => {
     </View>
   );
 };
+const styles = StyleSheet.create({});
