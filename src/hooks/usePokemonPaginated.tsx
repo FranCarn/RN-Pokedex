ce3af6f8f53c50c7003b859bb20fd95cccc4c52b
@FranCarn <carnevalefranco99@gmail.com>
2023-11-24 14:28:46 -0300

    infinite scroll ready

--- src/hooks/usePokemonPaginated.tsx
+++ src/hooks/usePokemonPaginated.tsx
@@ -42,5 +42,5 @@ export const usePokemonPaginated = () => {
     getInitialData();
   }, []);
 
-  return {isLoading, simplePokemonList};
+  return {isLoading, simplePokemonList, getInitialData};
 };
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,22 +1,42 @@
 import React from 'react';
-import {Text, Image} from 'react-native';
+import {Text, Image, FlatList, ActivityIndicator} from 'react-native';
 import {styles} from '../theme/appTheme';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {usePokemonPaginated} from '../hooks/usePokemonPaginated';
+import {} from 'react-native-gesture-handler';
 
 export const HomeScreen = () => {
   const {top} = useSafeAreaInsets();
-  const {} = usePokemonPaginated();
+  const {isLoading, simplePokemonList, getInitialData} = usePokemonPaginated();
   return (
     <>
       <Image
         source={require('../assets/pokebola.png')}
         style={styles.pokebolaBG}
       />
-
-      <Text style={{...styles.title, ...styles.globalMargin, top: top + 20}}>
-        Pokedex
-      </Text>
+      <FlatList
+        data={simplePokemonList}
+        keyExtractor={({id}) => id.toString()}
+        showsVerticalScrollIndicator={false}
+        renderItem={({item}) => (
+          <>
+            <Image
+              source={{uri: item.picture}}
+              style={{width: 100, height: 100}}
+            />
+            <Text>{item.name}</Text>
+          </>
+        )}
+        // infinite scroll
+        onEndReached={getInitialData}
+        onEndReachedThreshold={0.4}
+        ListFooterComponent={
+          <ActivityIndicator style={{height: 100}} size={20} color="grey" />
+        }
+      />
+      {/* <Text style={{...styles.title, ...styles.globalMargin, top: top + 20}}> */}
+      {/* Pokedex
+      </Text> */}
     </>
   );
 };
