9e88f3c445e0ac09e70c972b73b86992ae78433a
@FranCarn <carnevalefranco99@gmail.com>
2023-12-06 16:51:05 -0300

    pokemon screen skeleton ready

--- src/screens/PokemonScreen.tsx
+++ src/screens/PokemonScreen.tsx
@@ -1,8 +1,10 @@
 import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, TouchableOpacity, Image} from 'react-native';
 import {RootStackParams} from '../navigator/Navigator';
-import {Image} from 'react-native-reanimated/lib/typescript/Animated';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {FadeInImage} from '../components/FadeInImage';
 
 interface Props extends StackScreenProps<RootStackParams, 'PokemonScreen'> {}
 
@@ -11,11 +13,56 @@ export const PokemonScreen = ({navigation, route: {params}}: Props) => {
     pokemon: {id, name, picture},
     color,
   } = params;
+  const {top} = useSafeAreaInsets();
   return (
     <View>
-      <Text style={{backgroundColor: color}}>{name}</Text>
-      <Image source={{uri: picture}} />
+      <View style={{backgroundColor: color, ...styles.headerContainer}}>
+        <TouchableOpacity
+          activeOpacity={0.8}
+          style={{...styles.backButton, top: top + 5}}
+          onPress={() => navigation.pop()}>
+          <Icon name="arrow-back-outline" size={33} color="white" />
+        </TouchableOpacity>
+        <Text style={{...styles.pokemonName, top: top + 40}}>
+          {name + '\n'}#{id}
+        </Text>
+        <Image
+          source={require('../assets/pokebola-blanca.png')}
+          style={styles.pokeBall}
+        />
+        <FadeInImage uri={picture} style={styles.pokemonImage} />
+      </View>
     </View>
   );
 };
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  headerContainer: {
+    height: 370,
+    zIndex: 999,
+    alignItems: 'center',
+    borderBottomRightRadius: 1000,
+    borderBottomLeftRadius: 1000,
+  },
+  backButton: {
+    position: 'absolute',
+    left: 20,
+  },
+  pokemonName: {
+    color: 'white',
+    fontSize: 40,
+    alignSelf: 'flex-start',
+    left: 20,
+  },
+  pokeBall: {
+    width: 250,
+    height: 250,
+    bottom: -20,
+    opacity: 0.7,
+  },
+  pokemonImage: {
+    widht: 250,
+    height: 250,
+    position: 'absolute',
+    bottom: -15,
+  },
+});
